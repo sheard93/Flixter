64d4eae485bbacd47e66499a90ea9ffc152403b5
sheard93 <shelbyzaglin@gmail.com>
2019-09-27 16:48:20 +0000

    Listing the courses

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # hook up routes so the localhost:3030 takes us to the landing page
   root 'static_pages#index'
+  resources :courses, only: [:index, :show]
   namespace :instructor do
     resources :courses, only: [:new, :create, :show]
   end

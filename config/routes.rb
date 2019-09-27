f86d31be15c5fedb0e20c482610c342ca5045e42
sheard93 <shelbyzaglin@gmail.com>
2019-09-27 16:01:08 +0000

    Allow course creation

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # hook up routes so the localhost:3030 takes us to the landing page
   root 'static_pages#index'
+  namespace :instructor do
+    resources :courses, only: [:new, :create, :show]
+  end
 end

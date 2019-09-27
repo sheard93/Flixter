d2f7f3b083c8d2103dd742c507cc5f21273a7953
sheard93 <shelbyzaglin@gmail.com>
2019-09-27 16:08:21 +0000

    Course validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,9 @@
 class Course < ApplicationRecord
+  # Courses can be created by users
   belongs_to :user
+
+  # Validate that courses have a title, description, and cost
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
 end

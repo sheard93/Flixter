018176d82daac2ffde9d377c8f4927b56a1ee308
sheard93 <shelbyzaglin@gmail.com>
2019-09-27 20:19:55 +0000

    Issue with lesson details and image uploader

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'bootstrap', '~> 4.3.1'
 gem 'jquery-rails'
 gem 'simple_form'
 gem 'devise'
+gem 'carrierwave', '~> 2.0'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/course.rb
+++ app/models/course.rb
@@ -4,6 +4,8 @@ class Course < ApplicationRecord
   # Allows for multiple rows in sections table to have the same course id
   has_many :sections
 
+  mount_uploader :image, ImageUploader
+
   # Validate that courses have a title, description, and cost
   validates :title, presence: true
   validates :description, presence: true

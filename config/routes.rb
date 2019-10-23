206c12c47435c851fb0b145aa82e89e5e9a331d4
sheard93 <shelbyzaglin@gmail.com>
2019-10-23 18:37:30 +0000

    Adding a teacher dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resource :dashboard, only: [:show]
   # hook up routes so the localhost:3030 takes us to the landing page
   root 'static_pages#index'
   resources :courses, only: [:index, :show] do
